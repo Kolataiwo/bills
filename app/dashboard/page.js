d2ada8c953459a4bb192ac59f87ed6d9d5110306
Kolawole Abiola  Taiye <kolawoletaiye85@gmail.com>
2024-10-09 13:45:18 +0100

    design for dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,6 +3,10 @@ export default function dashboard (){
     return (
         <main>
             <p>user dashboard</p>
+            <p>propose a UI design</p>
+            <p>build the UI</p>
+            <p>propose possible changes</p>
+
         </main>
     )
 }
